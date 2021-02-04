f664ba2ff2d76dc9ea22ba7202d2a4520490b3b6
popatre <76402245+popatre@users.noreply.github.com>
2021-02-04 16:25:57 +0000

    completed more of the course
    completed more of the course

---
+++ embedding objects and arrays/myapp.js
@@ -0,0 +1,23 @@
+var computer = {
+  make: "apple",
+  model: "macbook pro",
+  year: 2015,
+  ram: [
+      "4gb",
+      "4gb",
+      "8gb",
+       ]
+
+
+};
+
+
+function upgrade() {
+
+  date = 2022;
+
+  return date;
+
+}
+
+console.log  ( upgrade () );
